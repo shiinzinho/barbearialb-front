ef8b28195d4ef18fd2b3c5721285a697b3f83787
shiinzinho <gabrieldosantoslacerda@gmail.com>
2023-10-26 15:17:25 -0300

    UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppRouter from './router/AppRouter';
-import CadastroCliente from './components/CadastroCliente';
 
 function App() {
   return (
-    <CadastroCliente />
+    <AppRouter />
   );
 }
 
